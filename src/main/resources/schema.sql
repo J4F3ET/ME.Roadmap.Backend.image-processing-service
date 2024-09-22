d4c378cec90aacf5b4669977069c480cf5b38c7f
J4F3ET <jslopezz@udistrital.edu.co>
2024-09-22 16:13:05 -0500

    feat: :sparkles: Agregar campos de Spring security al esquema de la base de datos

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,14 @@
 create table if not exists users (
     id serial primary key,
     username varchar(255) not null,
-    password varchar(255) not null
+    password varchar(255) not null,
+    is_enabled boolean not null,
+    is_account_no_expired boolean not null,
+    is_account_no_locked boolean not null,
+    is_credentials_no_expired boolean not null
 );
 
-create table if not exists images (
+create table if not exists image (
     id serial primary key,
     image_name varchar(255) not null,
     image_path varchar(255) not null,
