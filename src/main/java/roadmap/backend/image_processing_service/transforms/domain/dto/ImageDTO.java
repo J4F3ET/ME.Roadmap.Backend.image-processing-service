d60acbaeb06bf41f96c4429193dbe2b073550f17
J4F3ET <jslopezz@udistrital.edu.co>
2024-10-29 16:47:14 -0500

    refactor: :wrench: Cambio de record a class

--- src/main/java/roadmap/backend/image_processing_service/transforms/domain/dto/ImageDTO.java
+++ src/main/java/roadmap/backend/image_processing_service/transforms/domain/dto/ImageDTO.java
@@ -1,3 +1,15 @@
 package roadmap.backend.image_processing_service.transforms.domain.dto;
 
-public record ImageDTO(String name,String format,byte[] image) {}
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageDTO{
+    private String name;
+    private String format;
+    private byte[] image;
+}
